ec5b12678005842a7f3c0fbbafb3ca94ef477338
asfalfa <glkin9@gmail.com>
2024-08-19 14:24:34 +0200

    trying to make playwright play nice with vite

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'http://localhost:5173/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -71,8 +71,8 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:5000',
+    command: 'npx vite',
+    url: 'http://127.0.0.1:5173',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
